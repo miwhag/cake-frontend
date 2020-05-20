396f482c9af5eb90a05734b8eb68e9d86967c74e
github username <github email address>
2020-05-20 14:23:21 -0600

    add sign in bar to homepage navbar

---
+++ src/Components/Login/MiniLoginForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import '../../Styles/Signup/Signup.css'
+
+export default class MiniLoginForm extends Component {
+
+    state = {
+       password: '',
+       username: '',
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+          this.props.login(this.state)
+          .then(() => this.props.history.push('/build_your_cake'))
+    }
+    
+
+    render() {
+        return (
+            <form id="Mini-Login-form" onSubmit={this.handleSubmit}>
+                <input
+                    className="Mini-Signin-form-input" 
+                    name="username" 
+                    type ="text"
+                    placeholder="Username" 
+                    required
+                    value={this.state.username}
+                    onChange={this.handleChange}
+             >
+                </input>
+
+                <input
+                    className="Mini-Signin-form-input" 
+                    name="password" 
+                    type ="password"
+                    placeholder="Password" 
+                    required
+                    value={this.state.password}
+                    onChange={this.handleChange}
+             >
+                </input>
+
+                <button 
+                    className="Mini-Signin-form-submit" 
+                    type="submit" 
+                    value="Send">
+                        <i class="fas fa-sign-in-alt"></i>
+                </button>
+
+            </form>
+        )
+    }
+}
